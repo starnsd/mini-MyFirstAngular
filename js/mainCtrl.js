bb4e3250b3224bb35f6fd15f45a97aee010d36cc
DS <dillon.starns@activenetwork.com>
2016-09-24 10:41:03 -0500

    Done

---
+++ js/mainCtrl.js
@@ -0,0 +1,11 @@
+angular.module('friendsList').controller('mainCtrl', function($scope){
+    $scope.friends = [
+        "Joe Shmoe"
+        , "Fred Durst"
+        , "Dick Cheney"
+        , "Elvira"
+        , "Carly Simon"
+        , "Paul Rodriguez"
+        , "Alf"
+    ]
+});
